5743b1670432818df1e88e4faca27587701c90a0
Pedro juan <pedro.rodrigues353@aluno.ce.gov.br>
2026-09-16 11:22:16 +0000

    projeto-03

--- 3-bimestre/orientacao-a-objetos/projetos/projeto3/Notificacao.js
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto3/Notificacao.js
@@ -1,9 +1,36 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
 
+class Notificacao {
+    constructor(mensagem) {
+        this.mensagem = mensagem
+    }
 
+    enviar() {
+        throw new Error("O método enviar() deve ser implementado")
+    }
+}
 
+class Email extends Notificacao {
+    enviar() {
+        return `E-mail enviado: ${this.mensagem}`
+    }
+}
 
+class SMS extends Notificacao {
+    enviar() {
+        return `SMS enviado: ${this.mensagem}`
+    }
+}
 
+class App extends Notificacao {
+    enviar() {
+        return `Notificação no aplicativo: ${this.mensagem}`
+    }
+}
+
+module.exports = {
+    Notificacao, Email, SMS, App
+}
 
 // === FIM DO CÓDIGO =======================
 // === NÃO FAZER NADA ABAIXO DESSA LINHA ===
